def5aa44f8ea9e437e0cf202727a58ad1b62bfc1
Marc Steele <marcsteele2014@gmail.com>
2020-10-12 14:13:57 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash =
  		{
@@ -84,9 +83,11 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {}
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto] = {
+   [:known_for] => "Ruby",
+    [:languages] => ["LISP", "C"]
+ }
+programmer_hash
 end
 
 def changing_alan
@@ -107,8 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
-
+programmer_hash[:alan_kay][:known_for]= alans_new_info
+programmer_hash
 end
 
 def adding_to_dennis
@@ -128,4 +129,6 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
